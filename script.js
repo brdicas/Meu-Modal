08b2920af18c126d10e04155fe4e564b80bc2065
brdicas <155032371+brdicas@users.noreply.github.com>
2025-02-05 19:59:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(function() {
             modal.style.display = "block";
             spanClosee.style.display = "block"; // Mostra o botão de fechar
-        }, 10000); // 10000 milissegundos = 10 segundos
+        }, 50000); // 10000 milissegundos = 10 segundos
     }
 
     // Quando o usuário clica no "x", fecha o modal
